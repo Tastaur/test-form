84c4e24ca734fbad8b49cbbba94cc8d8c93270e3
Mark <Trrrrex93@gmail.com>
2020-06-17 01:09:45 +0400

    Updates

---
+++ precache-manifest.ae607b3852a642ed2090c51f0fbe1e1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588ba60b3c31cfcf9c094c6a4c698bfc",
+    "url": "/test-form/index.html"
+  },
+  {
+    "revision": "5dae2a58c1fbaccbd441",
+    "url": "/test-form/static/css/main.9ac56d77.chunk.css"
+  },
+  {
+    "revision": "1e9dfcb19d26f0e6db97",
+    "url": "/test-form/static/js/2.c63ee2a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-form/static/js/2.c63ee2a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dae2a58c1fbaccbd441",
+    "url": "/test-form/static/js/main.65b7e478.chunk.js"
+  },
+  {
+    "revision": "6b7ea56ea7348f77b72d",
+    "url": "/test-form/static/js/runtime-main.28712ef2.js"
+  },
+  {
+    "revision": "f25fd40461c2a64bd191479ca199f9a2",
+    "url": "/test-form/static/media/BebasNeue Regular.f25fd404.ttf"
+  }
+]);
